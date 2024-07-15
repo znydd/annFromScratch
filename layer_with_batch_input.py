3d4f8b607341685a83f832f46ce4a8b6e8363603
Nabil <mailboxofnabil@gmail.com>
2024-07-16 03:39:58 +0600

    batch input in neuron layer

---
+++ layer_with_batch_input.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Batch of input
+inputs = [[1.0, 2.0, 3.0, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
+
+weights = [[0.2, 0.8, -0.5, 1],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases =[2.0,3.0, 0.5]
+
+outputs = np.dot(inputs, np.array(weights).T) + biases
+
+print(outputs)
+
