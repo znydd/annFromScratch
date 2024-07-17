be3db7ef520dd06c5448fe6919d2d64e694f36b1
Nabil <mailboxofnabil@gmail.com>
2024-07-18 02:01:51 +0600

    Added Softmax activation func class

--- Dense_layer_Class_babe.py
+++ Dense_layer_Class_babe.py
@@ -4,15 +4,13 @@ from nnfs.datasets import spiral_data
 
 nnfs.init()
 
-class Layer_Dense:
-    
+class Layer_Dense:    
     def __init__(self, n_inputs, n_neurons):
         # weight and biases initialization
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons) # we are doing (inputs x neurons) instead (neurons x inputs) 
                                                                   # so that don't have to calculate transpose every time
         self.biases = np.zeros((1, n_neurons))
-        
-    
+            
     def forward(self, inputs):
         # Calculate output values from inputs, weights, and biases
         self.output = np.dot(inputs, self.weights) + self.biases
@@ -26,6 +24,17 @@ class Acivation_ReLU:
         self.output = np.maximum(0, inputs)
 
 
+class Activation_Softmax:
+    def __init__(self) -> None:
+        pass
+    
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True)) # To prevent neuron exploit we are subtracting the max
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+
 
 X, y = spiral_data(samples=100, classes=3)
 
@@ -33,12 +42,20 @@ dense1 = Layer_Dense(2,3)
 
 activation1 = Acivation_ReLU()
 
+dense2 = Layer_Dense(3, 3)
+
+activation2 = Activation_Softmax()
+
 # First through neuron
 dense1.forward(X)
 # Then through activation function
 activation1.forward(dense1.output)
 
-print(activation1.output[:5])
+dense2.forward(activation1.output)
+
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
 
 
 
