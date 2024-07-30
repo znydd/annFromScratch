42f52d37add48bda39e3dab90b9ba1b91ffb7541
Znyd <mailboxofnabil@gmail.com>
2024-07-31 00:22:37 +0600

    accuracy with pred vs target mean

---
+++ accuracy.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+softmax_outputs = np.array([[0.7, 0.2, 0.1],
+                            [0.5, 0.1, 0.4],
+                            [0.02, 0.9, 0.08]])
+
+class_targets = np.array([0, 1, 1])
+
+predictions = np.argmax(softmax_outputs, axis=1)
+
+#If targets are OHE
+if len(class_targets.shape) == 2:
+    class_targets = np.argmax(class_targets, axis=1)
+
+accuracy = np.mean(predictions==class_targets)
+
+print('acc:', accuracy)
